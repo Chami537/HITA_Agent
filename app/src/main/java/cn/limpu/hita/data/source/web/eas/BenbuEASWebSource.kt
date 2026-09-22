ab92bf6ce57fbdfd5b2d9002e00b681886d8a179
Chami537 <Rinatsuko@proton.me>
2026-09-22 20:14:18 +0800

    fix: correct graduate afternoon timetable periods

--- app/src/main/java/cn/limpu/hita/data/source/web/eas/BenbuEASWebSource.kt
+++ app/src/main/java/cn/limpu/hita/data/source/web/eas/BenbuEASWebSource.kt
@@ -1028,14 +1028,14 @@ class BenbuEASWebSource(
             TimePeriodInDay(TimeInDay(8, 55), TimeInDay(9, 45)),
             TimePeriodInDay(TimeInDay(10, 0), TimeInDay(10, 50)),
             TimePeriodInDay(TimeInDay(10, 55), TimeInDay(11, 45)),
-            TimePeriodInDay(TimeInDay(13, 45), TimeInDay(14, 35)),
-            TimePeriodInDay(TimeInDay(14, 40), TimeInDay(15, 30)),
-            TimePeriodInDay(TimeInDay(15, 45), TimeInDay(16, 35)),
-            TimePeriodInDay(TimeInDay(16, 40), TimeInDay(17, 30)),
-            TimePeriodInDay(TimeInDay(18, 30), TimeInDay(19, 20)),
-            TimePeriodInDay(TimeInDay(19, 25), TimeInDay(20, 15)),
-            TimePeriodInDay(TimeInDay(20, 30), TimeInDay(21, 20)),
-            TimePeriodInDay(TimeInDay(21, 25), TimeInDay(22, 15))
+            TimePeriodInDay(TimeInDay(14, 0), TimeInDay(14, 50)),
+            TimePeriodInDay(TimeInDay(14, 55), TimeInDay(15, 45)),
+            TimePeriodInDay(TimeInDay(16, 0), TimeInDay(16, 50)),
+            TimePeriodInDay(TimeInDay(16, 55), TimeInDay(17, 45)),
+            TimePeriodInDay(TimeInDay(18, 45), TimeInDay(19, 35)),
+            TimePeriodInDay(TimeInDay(19, 40), TimeInDay(20, 30)),
+            TimePeriodInDay(TimeInDay(20, 45), TimeInDay(21, 35)),
+            TimePeriodInDay(TimeInDay(21, 40), TimeInDay(22, 30))
         )
     }
 
